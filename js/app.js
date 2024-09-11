4f13a13274d92bf11b591f53ad7fe875ee96954f
NataliadePaula <nataliadepaulasantos@gmail.com>
2024-09-11 10:15:43 -0300

    Substituindo array por variável

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 let item = document.getElementById("lista-produtos");
 let total = document.getElementById("valor-total");
-let valorAdd = [];
+let precoTotal = 0;
 
 
 function adicionar(){
@@ -12,24 +12,18 @@ function adicionar(){
     item.innerHTML = item.innerHTML + `<section class="carrinho__produtos__produto">
     <span class="texto-azul">${quantidade}x</span> ${nomeItem}<span class="texto-azul">R$${valorTotalItem}</span>
   </section>`;
-    valorAdd.push(valorTotalItem);
-    let precoTotal = 0;
-    for(let i = 0; i <valorAdd.length; i++) {
-        precoTotal += valorAdd[i];
-    }
+    precoTotal = precoTotal + valorTotalItem;
     total.innerHTML = `R$ ${precoTotal}`;
     document.getElementById("quantidade").value = "";
 }
  
 
-
-
 function limpar(){
     item.innerHTML = "";
     total.innerHTML = "";
-    let quantidade = document.getElementById("quantidade")
-    quantidade.value = "";
-    valorAdd.length = 0;
+    document.getElementById("quantidade").value = "";
+    //quantidade.value = "";
+    precoTotal = 0;
    
 }
 
