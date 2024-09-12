b3f4d17cf49d8a7759f3ff28913f100cebd18668
NataliadePaula <nataliadepaulasantos@gmail.com>
2024-09-12 15:47:42 -0300

    Validação de qtd, criação da função limparQtd

--- js/app.js
+++ js/app.js
@@ -8,23 +8,31 @@ function adicionar(){
     let quantidade = document.getElementById("quantidade").value; 
     let valorUnitario = produto.split("R$")[1];
     let nomeItem = produto.split("-")[0];
-    let valorTotalItem = valorUnitario*quantidade;    
+    let valorTotalItem = valorUnitario*quantidade;
+    if(document.getElementById("quantidade").value <= 0){  
+      alert("Quantidade inválida. Verifique e tente novamente!");
+      limparQtd();
+      return;
+    }
     item.innerHTML = item.innerHTML + `<section class="carrinho__produtos__produto">
     <span class="texto-azul">${quantidade}x</span> ${nomeItem}<span class="texto-azul">R$${valorTotalItem}</span>
   </section>`;
     precoTotal = precoTotal + valorTotalItem;
     total.innerHTML = `R$ ${precoTotal}`;
-    document.getElementById("quantidade").value = "";
+    limparQtd();
 }
  
 
 function limpar(){
     item.innerHTML = "";
     total.innerHTML = "";
-    document.getElementById("quantidade").value = "";
+    limparQtd();
     //quantidade.value = "";
     precoTotal = 0;
    
 }
 
+function limparQtd(){
+  document.getElementById("quantidade").value = "";
+}
 document.addEventListener("DOMContentLoaded", limpar());
